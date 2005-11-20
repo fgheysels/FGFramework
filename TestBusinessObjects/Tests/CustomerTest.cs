813ca6d15e7cef8f722c9d5b8f5b9c94b57ae23b
frederik.gheysels <frederik.gheysels@telenet.be>
2005-11-20 13:13:44 +0100

    ﻿ [git-vault-id] FGFramework$/FGFramework@66/867

---
+++ TestBusinessObjects/Tests/CustomerTest.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using TestBusinessObjects.Repositories;
+using NUnit.Framework;
+
+namespace TestBusinessObjects.Tests
+{
+    [TestFixture]
+    class CustomerTest
+    {
+        [Test]
+        public void TestLazyOrders()
+        {
+            CustomerRepository cr = new CustomerRepository ();
+
+            Customer c = cr.GetCustomer (1);
+            
+            Assert.AreEqual ("frederik", c.Name.ToLower (), "Wrong customer");
+
+            Assert.AreEqual (2, c.Orders.Count, "Number of orders is not ok.");
+        }
+    }
+}
