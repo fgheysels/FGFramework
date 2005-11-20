5d9edf02b5ee9d0c2c178645ac12dc9680f74ce0
frederik.gheysels <frederik.gheysels@telenet.be>
2005-11-20 13:13:44 +0100

    ﻿ [git-vault-id] FGFramework$/FGFramework@31/832

---
+++ BusinessObjects/LazyBusinessObjectCollection.cs
@@ -0,0 +1,149 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessObjects
+{
+    /// <summary>
+    /// LazyBusinessObjectCollection provides the functionality of a collection, but,
+    /// the collection is only populated once it's first accessed.
+    /// It is a virtual proxy.
+    /// </summary>
+    /// <typeparam name="T">The type of the specific BusinessObjects that will be contained
+    /// in this collection.</typeparam>
+    public class LazyBusinessObjectCollection<T> : IUndoable where T : BusinessObject
+    {
+        /// <summary>
+        /// The ILazyLoader that will get the objects that will be contained in
+        /// this collection, once the lazycollection is accessed for the first time.
+        /// </summary>
+        private ILazyLoader<T> _loader;
+
+        /// <summary>
+        /// The BusinessObjectCollection where this LazyCollection is a wrapper (proxy) for.        
+        /// </summary>
+        /// <remarks>Do not access this field directly, use the CollectionObj property instead.</remarks>
+        private BusinessObjectCollection<T> _collection = null;
+
+        /// <summary>
+        /// The property accessor that must be used when you 
+        /// </summary>
+        private BusinessObjectCollection<T> CollectionObj
+        {
+            get
+            {
+                if( _collection == null )
+                {
+                    // Initialize the collection...
+                    _collection = new BusinessObjectCollection<T> ();
+
+                    // Get the objects and add them to the collection.
+                    foreach( T item in _loader.GetObjects () )
+                    {
+                        _collection.Add (item);
+                    }
+
+                }
+                return _collection;
+            }
+        }
+
+        #region Constructor
+
+        /// <summary>
+        /// Creates an instance of a LazyBusinessObjectCollection.
+        /// </summary>
+        /// <param name="loader">An ILazyLoader that will be used to populate the lazy collection.</param>
+        public LazyBusinessObjectCollection( ILazyLoader<T> loader )
+        {
+            // Make sure that the loader argument is not null, since we need 
+            // a valid loader to be able to populate the collection.
+            if( loader == null )
+            {
+                throw new InvalidOperationException ("The ILazyLoader<T> argument cannot be null.");
+            }
+            _loader = loader;
+        }
+
+        #endregion
+
+        #region Collection interface 
+
+        public bool IsDirty
+        {
+            get
+            {
+                return CollectionObj.IsDirty;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return CollectionObj.Count;
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return CollectionObj[index];
+            }
+        }
+
+        public int Add( T item )
+        {
+            return CollectionObj.Add (item);
+        }
+
+        public void Insert( int index, T item )
+        {
+            CollectionObj.Insert (index, item);
+        }
+
+        public void Remove( T item )
+        {
+            CollectionObj.Remove (item);
+        }
+
+        public void RemoveAt( int index )
+        {
+            CollectionObj.RemoveAt (index);
+        }
+
+        public T[] GetDeletedBusinessObjects()
+        {
+            return CollectionObj.GetDeletedBusinessObjects ();
+        }
+
+        public void ClearDeleted()
+        {
+            CollectionObj.ClearDeleted ();
+        }
+
+        #endregion
+
+        #region IUndoable Members
+
+        public void CreateSnapshot()
+        {
+            // Delegate the call to the collection itself; if the collection is not 
+            // loaded yet, it will be loaded now.
+            CollectionObj.CreateSnapshot ();
+        }
+
+        public void CommitSnapshot()
+        {
+            CollectionObj.CommitSnapshot ();
+        }
+
+        public void RevertToPreviousState()
+        {
+            CollectionObj.RevertToPreviousState ();
+        }
+
+        #endregion
+    }
+}
